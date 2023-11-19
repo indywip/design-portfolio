68b9602fcb691169c7ade2adef27af41ebcf5b39
Indy Sowy <indira.sowy@gmail.com>
2023-11-18 21:48:44 -0800

    update about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -21,7 +21,7 @@ const About = () => {
           <Content>
             <Center>
             <Header>Meet Indira Sowy</Header>
-            <Sub>I am a product designer, developer, and manager based in Vancouver, currently a 
+            <Sub>I am a product designer and developer based in Vancouver, currently a 
               second year studying under the Faculty of Arts at the University of British Columbia. 
               As someone who’s been doing design since high school, creation is a passion close to my heart. 
               I hope to use my skills to help develop the future of the tech industry.
